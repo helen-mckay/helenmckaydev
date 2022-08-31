af97049d1d5ba39f1f3a4b9efe15102f8bd273b8
Helen McKay <helen.mckay@comcast.net>
2022-08-31 15:01:12 -0500

    trying to get image to show on about

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('assets');
     eleventyConfig.addPassthroughCopy('images');
     eleventyConfig.addPassthroughCopy('js');
+    eleventyConfig.addPassthroughCopy('assets/images');
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
     eleventyConfig.addPlugin(pluginDate, {
         formats: {
