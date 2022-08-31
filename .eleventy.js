7eb22ff6ab98b595e9326cc024fd4c0d9ee8da52
Helen McKay <helen.mckay@comcast.net>
2022-08-31 15:41:11 -0500

    removed passthroughcopy to assets/images

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,6 @@ const pluginDate = require("eleventy-plugin-date");
 
 module.exports = function (eleventyConfig) {    
     eleventyConfig.addPassthroughCopy('assets');
-    eleventyConfig.addPassthroughCopy('./assets/images');
-    eleventyConfig.addPassthroughCopy('./assets/images/uploads');
     eleventyConfig.addPassthroughCopy('images');
     eleventyConfig.addPassthroughCopy('js');
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
